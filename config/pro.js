f7cba9e3f9304794789ae6d65bff2f4403d3e6bb
mustufa khan <mohdmustufa568@gmail.com>
2021-01-29 21:59:52 +0530

    make it better

--- config/pro.js
+++ config/pro.js
@@ -1,5 +1,5 @@
 module.exports={
-    MONGOURI:`${process.env.MOGOURI}`,
+    MONGOURI:`${process.env.MONGOURI}`,
     JWT_SECRET:`${process.env.JWT_SECRET}`,
     SENDGRID_API:`${process.env.SENDGRID_API}`,
     EMAIL:`${process.env.EMAIL}`
